267a8bcc93b73be74d7bbc284029d0158e86cbc5
jamescw <jcranwellward@gmail.com>
2014-07-04 14:55:53 +0300

    bumped version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='activityinfo_python',
-    version='1.2',
+    version='1.3',
     author='James Cranwell-Ward',
     author_email='jcranwellward@unicef.org',
     py_modules=['activtyinfo_client'],
