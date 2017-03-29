b8690493ea01c1dfa616d6d31c40fe09d41d8175
Maarten-Jan Kallen <mj@bedatadriven.com>
2017-03-29 19:47:49 +0200

    Fixed typo in module name
    An import like `from activityinfo_client import ActivityInfoClient` doesn't work because of a typo in the module name.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.5.0',
     author='James Cranwell-Ward',
     author_email='jcranwellward@unicef.org',
-    py_modules=['activtyinfo_client'],
+    py_modules=['activityinfo_client'],
     install_requires=['requests'],
     description='Simple python wrapper for ActivityInfo REST API'
 )
