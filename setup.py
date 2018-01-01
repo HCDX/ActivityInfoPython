5839260a36e85956964e6d2f61ec881c919eca99
James Cranwell-Ward <jcranwellward@gmail.com>
2018-01-01 18:56:17 +0200

    Distribution files

--- setup.py
+++ setup.py
@@ -7,5 +7,9 @@ setup(
     author_email='jcranwellward@unicef.org',
     py_modules=['activityinfo_client'],
     install_requires=['requests'],
-    description='Simple python wrapper for ActivityInfo REST API'
+    description='Simple python wrapper for ActivityInfo REST API',
+    url='https://github.com/HCDX/ActivityInfoPython',
+    download_url='https://github.com/HCDX/ActivityInfoPython/archive/v1.6.0.tar.gz',
+    keywords=['activityinfo', 'api', 'requests', 'python'],
+    classifiers = [],
 )
